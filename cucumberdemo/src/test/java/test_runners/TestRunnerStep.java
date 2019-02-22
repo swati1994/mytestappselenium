aa7b39ea8cdc582a14423982f5b39398b630ad71
swati1994 <tiwari.payal.swati@gmail.com>
2019-02-22 15:11:55 +0530

    modified the path

--- cucumberdemo/src/test/java/test_runners/TestRunnerStep.java
+++ cucumberdemo/src/test/java/test_runners/TestRunnerStep.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\jenkins_workspace\\cucumberdemo\\src\\test\\java",
+@CucumberOptions(features="C://cucumberdemo//src//test//java",
                  glue={"step_defs"},
                 		 plugin = { "pretty", "json:target/cucumber-reports/Cucumber.json" },
                 		 monochrome = true)
